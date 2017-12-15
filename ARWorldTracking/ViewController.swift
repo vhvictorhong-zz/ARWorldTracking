597546cd0391e7de423849e7fdd64aebb3c52a8c
victorhong1988 <victorhong1988@gmail.com>
2017-12-15 17:02:19 -0500

    Made house node

--- ARWorldTracking/ViewController.swift
+++ ARWorldTracking/ViewController.swift
@@ -30,15 +30,43 @@ class ViewController: UIViewController {
 
     @IBAction func addAction(_ sender: Any) {
         let node = SCNNode()
+        let boxNode = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0))
+        boxNode.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        let doorNode = SCNNode(geometry: SCNPlane(width: 0.03, height: 0.06))
+        doorNode.geometry?.firstMaterial?.diffuse.contents = UIColor.brown
+//        let cylinderNode = SCNNode(geometry: SCNCylinder(radius: 0.05, height: 0.05))
+//        cylinderNode.geometry?.firstMaterial?.specular.contents = UIColor.red
+//        node.geometry = SCNCone(topRadius: 0.1, bottomRadius: 0.1, height: 0.3)
 //        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.03)
-//        node.geometry?.firstMaterial?.specular.contents = UIColor.white
-//        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
-        node.geometry = SCNCapsule(capRadius: 0.1, height: 0.1)
-        let x = randomNumbers(firstNum: -0.3, secondNum: 0.3)
-        let y = randomNumbers(firstNum: -0.3, secondNum: 0.3)
-        let z = randomNumbers(firstNum: -0.3, secondNum: 0.3)
-        node.position = SCNVector3(0, 0, -0.3)
+//        node.geometry = SCNCylinder(radius: 0.2, height: 0.2)
+//        node.geometry = SCNSphere(radius: 0.2)
+//        node.geometry = SCNTube(innerRadius: 0.2, outerRadius: 0.3, height: 0.5)
+//        node.geometry = SCNTorus(ringRadius: 0.3, pipeRadius: 0.1)
+//        node.geometry = SCNPlane(width: 0.2, height: 0.2)
+        node.geometry = SCNPyramid(width: 0.1, height: 0.1, length: 0.1)
+//        let path = UIBezierPath()
+//        path.move(to: CGPoint(x: 0, y: 0))
+//        path.addLine(to: CGPoint(x: 0, y: 0.2))
+//        path.addLine(to: CGPoint(x: 0.2, y: 0.3))
+//        path.addLine(to: CGPoint(x: 0.4, y: 0.2))
+//        path.addLine(to: CGPoint(x: 0.4, y: 0))
+//        let shape = SCNShape(path: path, extrusionDepth: 0.2)
+//        node.geometry = shape
+        node.geometry?.firstMaterial?.specular.contents = UIColor.white
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+//        let x = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+//        let y = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+//        let z = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+        node.position = SCNVector3(0.2, 0.3, -0.2)
+        boxNode.position = SCNVector3(0, -0.05, 0)
+        doorNode.position = SCNVector3(0, -0.02, 0.053)
+//        cylinderNode.position = SCNVector3(-0.3, 0.2, -0.3)
         self.sceneView.scene.rootNode.addChildNode(node)
+        node.addChildNode(boxNode)
+        boxNode.addChildNode(doorNode)
+//        node.addChildNode(cylinderNode)
+//        self.sceneView.scene.rootNode.addChildNode(cylinderNode)
+
     }
     
     @IBAction func resetSession(_ sender: Any) {
